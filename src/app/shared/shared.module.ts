56711b37f6aae3cfd9de433d05413e36f1dbd0a7
Nathanael MU <nathanaelmayaunglat@gmail.com>
2021-12-09 19:45:22 +0100

    adds boilerplate code - navbar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+//Material Imports
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+
+
+
+
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ...materialModules,
+    CommonModule
+  ],
+  exports: [
+    ...materialModules
+  ]
+})
+export class SharedModule { }
